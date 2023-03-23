783d7c3ecb8064510cd81a537e901b11ccf05ff8
杨宏超 <9469647+vaeyhc@user.noreply.gitee.com>
2023-03-23 22:49:31 +0800

    first commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Home</h1>
+  <h2>{{ xianshi }}</h2>
+    <el-button type="primary" @click="luoli">获取</el-button>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+
+let  xianshi = ref('aa')
+
+
+let luoli = function(){
+    // axios.get('/users').then(function(req,res){
+    //       console.log(req.data);
+    //       xianshi.value = req.data
+    // },
+    
+    // function(error){
+    //   console.log(error.message);
+    // })
+}
+  
+</script>
+
+<style>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+ 
+  devServer:{
+    // 反向代理
+    proxy : {
+      "/adminapi" :{
+        target : "http://localhost:3000",
+        changeOrigin : true,
+      },
+      
+    },
+
+    host: '0.0.0.0',
+    port:5050,
+    client: {
+      webSocketURL: 'ws://0.0.0.0:5050/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
+})
+
